962f45b3626b325d5cf9a9c98197ea9d4286f527
Nino Vrijman <ninovrijman@gmail.com>
2019-02-09 16:09:13 +0100

    Improved log messages, added chalk and ora to display colors and spinners

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
+const ora = require('ora');
+const chalk = require('chalk');
+
 const CITY_ID = 3974;
 const BEGINNING_OF_TIME = '2014-02-05';
 const searchQueries = ['Popal'];
+
+const WORD_ART = (() => {
+  const title = 'HAS MY BIKE BEEN FOUND?'
+  const pad = (amount, character) => `${character || ' '}`.repeat(amount);
+  const LINE_LENGTH = 8 + title.length + 8;
+  return `${pad(LINE_LENGTH)}
+${pad((LINE_LENGTH - title.length) / 2)}${title}${pad((LINE_LENGTH - title.length) / 2)}
+${pad(17)}(probably not)${pad(8)}
+${pad(LINE_LENGTH)}`;
+})();
 
 const generateUrl = ({
   cityId,
@@ -15,18 +28,21 @@ const generateUrl = ({
    * Example url for given search query in city on the current date:
    * https://www.verlorenofgevonden.nl/gevonden-voorwerpen?q=&date=2019-02-05&city=3974
    */
-  if (doNationWideSearch && cityId) {
-    console.log(
-      'Both cityId and doNationWideSearch were specified, executing nationwide search...'
-    );
-  }
   return `https://www.verlorenofgevonden.nl/gevonden-voorwerpen?${
     doNationWideSearch ? '' : `city=${cityId}`
   }&q=${searchQuery}&date=${date}`;
 };
 
 (async () => {
+  console.log(chalk.black.bgYellow(WORD_ART));
+  console.log(chalk.dim(`Last search was done on: ${new Date()}\n`));
+
+  const launchingBrowserSpinner = ora(
+    `Searching for: '${searchQueries.join("', '")}'`
+  ).start();
   const browser = await puppeteer.launch();
+  launchingBrowserSpinner.succeed();
+  const resultsSpinner = ora('Determining how many results to scrape').start();
   const page = await browser.newPage();
   await page.goto(
     generateUrl({
@@ -50,26 +66,33 @@ const generateUrl = ({
     throw new Error("Couldn't determine amount of results");
   }
 
+  resultsSpinner.succeed();
+  ora().info(`Website will be scraped for: ${amountOfResults} results`);
+  const lazyLoadingSpinner = ora({
+    text: `Scraping website for: ${amountOfResults} results`
+  }).start();
+
   /**
    * Handle lazy loading: https://intoli.com/blog/scrape-infinite-scroll/
    */
   let previousScrollHeight;
-  while (
-    (await page.evaluate(
-      () => document.querySelectorAll('.search-results li').length
-    )) < amountOfResults
-  ) {
+  let found = 0;
+  while (found < amountOfResults) {
     previousScrollHeight = await page.evaluate('document.body.scrollHeight');
     await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
     await page.waitForFunction(
       `document.body.scrollHeight > ${previousScrollHeight}`
     );
+    found = await page.evaluate(
+      () => document.querySelectorAll('.search-results li').length
+    );
+    lazyLoadingSpinner.text = `Scraping website for: ${amountOfResults} results (${found}/${amountOfResults})`;
   }
 
   const $ = cheerio.load(await page.content());
 
   if ($('.search-results li').length === amountOfResults) {
-    console.log(`Found all ${amountOfResults} results!`);
+    lazyLoadingSpinner.succeed(`${amountOfResults} results scraped successfully!`);
     await browser.close();
   } else {
     process.on('exit', async () => await browser.close());
